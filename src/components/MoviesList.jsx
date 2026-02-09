6066f9d3d7e2f6fa6d5f6a11ea75d8e531d3cc52
Suso777 <xesussuarez@gmail.com>
2026-02-10 00:25:57 +0100

    add import css

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,6 +1,7 @@
+import "./MoviesList.css";
 function MoviesList({ movies }) {
   return (
-    <ul>
+    <ul className="movies-grid">
       {movies.map((movie) => (
         <li key={movie._id}>{movie.name}</li>
       ))}
